d9704d434eadeb2590ee8840b8125146f2e041e0
hwolf0610 <hwolf0610@hotmail.com>
2021-01-13 18:26:03 +0800

    update enviorment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-   production: false,
-   API_ENDPOINT: "https://ocean-a-pi-6oakj.ondigitalocean.app",
+   production: true,
+   API_ENDPOINT: "https://backend-api.ethamara.com",
    GOOGLE_MAP_API_KEY: "AIzaSyAjz0Hmtl8wG08ljvkUGProhdxGtIyJtlk",
    STRIPE_PUBLISHABLE_KEY: "fc43f3f3",
-   IMAGEKIT_URL: "https://ik.imagekit.io/softylus/ethmar",
-   PREDEFINED: true
-};
+   IMAGEKIT_URL: "https://ik.imagekit.io/tw3050qsyxo/",
+   PREDEFINED: false
+   };
